3864068c752ec2e09f8ada1b9bb173921b39e21c
rustamchik <rstmsarmanov@gmail.com>
2026-08-25 12:49:25 +0500

    Configure Firebase guest storage

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,13 @@
 // Вставьте сюда конфигурацию веб-приложения из Firebase Console.
 // Firebase config можно безопасно размещать во frontend-коде: доступ защищают firestore.rules.
 export const firebaseConfig = {
-  apiKey: "PASTE_API_KEY_HERE",
-  authDomain: "PASTE_PROJECT_ID.firebaseapp.com",
-  projectId: "PASTE_PROJECT_ID",
-  storageBucket: "PASTE_PROJECT_ID.firebasestorage.app",
-  messagingSenderId: "PASTE_MESSAGING_SENDER_ID",
-  appId: "PASTE_APP_ID"
+  apiKey: "AIzaSyAxX7-h7h2OalHgHF0reJbXK8rTdTRtRj8",
+  authDomain: "yubiley-44408.firebaseapp.com",
+  projectId: "yubiley-44408",
+  storageBucket: "yubiley-44408.firebasestorage.app",
+  messagingSenderId: "79478054447",
+  appId: "1:79478054447:web:fcb71e1a20e77f3d8570da",
+  measurementId: "G-W7CNQ3N4TG"
 };
 
 export const isFirebaseConfigured = !Object.values(firebaseConfig).some((value) => value.startsWith("PASTE_"));
